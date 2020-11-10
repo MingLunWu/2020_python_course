898b6e6509676d44d3781cb5f4acb8e750ffcbf5
MingLun Wu <allen6997535@gmail.com>
2020-11-10 23:39:38 +0800

    Upload Week5 material

---
+++ Week5/my_package/calculate.py
@@ -0,0 +1,3 @@
+def calculate_BMI(None, None) # 這裡請放兩個參數height, weight
+    myBMI = None # 請針對輸入的參數進行計算
+    return None # 回傳計算好的BMI
---
+++ Week5/my_package/get_data.py
@@ -0,0 +1,10 @@
+def get_sample_data():
+# 此函式的目的為產生範例字典
+    income = {
+        "Monday":[20,10,30,40,20],
+        "Tuesday":[50,60,20,10,100],
+        "Wednesday":[20,70,70,100,20],
+        "Thursday":[80,0,20,20,50],
+        "Friday":[100,20,80,20,10]
+    }
+    return income
